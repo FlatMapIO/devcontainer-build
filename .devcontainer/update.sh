05d5ce1d1a4829984ddc11e60c2d99bc940316be
dong.huo <huodon@gmail.com>
2023-09-21 22:58:19 +0800

    [skip ci]

--- .devcontainer/update.sh
+++ .devcontainer/update.sh
@@ -4,4 +4,6 @@ echo "== Set pip cn mirror =="
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 
 echo "== Set apt cn mirror =="
-sed -i 's/archive.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g' /etc/apt/sources.list
+
+# TODO this command need sudo permission
+# sed -i 's/archive.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g' /etc/apt/sources.list
