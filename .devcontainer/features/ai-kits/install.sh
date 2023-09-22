2a6837a54b23ec1b7cff059c2a91a7b264e9f703
dong.huo <huodon@gmail.com>
2023-09-22 14:51:59 +0800

    UP

--- .devcontainer/features/ai-kits/install.sh
+++ .devcontainer/features/ai-kits/install.sh
@@ -1,4 +1,34 @@
-source ./py-utils.sh
+USERNAME="${USERNAME:-"${_REMOTE_USER:-"automatic"}"}"
+
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=root
+    fi
+elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
+    USERNAME=root
+fi
+
+sudo_if() {
+    COMMAND="$*"
+    if [ "$(id -u)" -eq 0 ] && [ "$USERNAME" != "root" ]; then
+        su - "$USERNAME" -c "$COMMAND"
+    else
+        "$COMMAND"
+    fi
+}
+
+install_user_package() {
+    PACKAGE="$1"
+    sudo_if "${PYTHON_PATH}/bin/python3" -m pip install --user --upgrade -v --no-cache-dir "$PACKAGE"
+}
 
 install_user_package \
     --index-url ${INDEXURL} \
