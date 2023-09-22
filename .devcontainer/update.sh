4de6fe6f380c4738123591793aab466921fc4c5a
dong.huo <huodon@gmail.com>
2023-09-22 21:11:22 +0800

    [skip ci]

--- .devcontainer/update.sh
+++ .devcontainer/update.sh
@@ -2,6 +2,9 @@ echo "=========== Update you container on start ==========="
 
 echo "== Set pip cn mirror =="
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+if [ -f ./requirements.txt ]; then
+  pip install -r ./requirements.txt
+fi
 
 echo "== Set apt cn mirror =="
 
