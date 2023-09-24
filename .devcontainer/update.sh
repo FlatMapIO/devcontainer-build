89b3d5d6121b104c65c38c80bec60c96d2fbb1b1
dong.huo <huodon@gmail.com>
2023-09-24 10:23:09 +0800

    [skip ci]

--- .devcontainer/update.sh
+++ .devcontainer/update.sh
@@ -1,12 +1,14 @@
 echo "=========== Update you container on start ==========="
 
+
 echo "== Set pip cn mirror =="
+# TODO check gepip
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+
+
 if [ -f ./requirements.txt ]; then
   pip install -r ./requirements.txt
 fi
 
-echo "== Set apt cn mirror =="
-
-# TODO this command need sudo permission
-# sed -i 's/archive.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g' /etc/apt/sources.list
+echo "Install Mojo: https://developer.modular.com/download"
+echo "Use apt cn source: sed -i 's/archive.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g' /etc/apt/sources.list"
