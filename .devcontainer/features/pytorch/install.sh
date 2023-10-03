74af11af3a6766a3aa34126488587ea5dcd9fb91
dong.huo <huodon@gmail.com>
2023-10-03 13:07:41 +0800

    [skip ci]

--- .devcontainer/features/pytorch/install.sh
+++ .devcontainer/features/pytorch/install.sh
@@ -7,5 +7,6 @@ user_exec() {
 
 
 user_exec ${PYTHON_PATH}/bin/python3 -m pip install --user --upgrade --no-cache-dir -v \
+    ipywidgets \
     torch torchvision torchaudio --extra-index-url "${INDEXURL}" \
     xformers
