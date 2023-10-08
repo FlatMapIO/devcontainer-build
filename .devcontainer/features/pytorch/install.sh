d07b87ef6d96150394e6af2ee57409dbcf9e4d99
huodon <huodon@gmail.com>
2023-10-08 18:47:52 +0800

    WTF

--- .devcontainer/features/pytorch/install.sh
+++ .devcontainer/features/pytorch/install.sh
@@ -7,6 +7,8 @@ user_exec() {
 
 
 user_exec ${PYTHON_PATH}/bin/python3 -m pip install --user --upgrade --no-cache-dir -v \
-    ipywidgets \
     torch torchvision torchaudio --extra-index-url "${INDEXURL}" \
+
+user_exec ${PYTHON_PATH}/bin/python3 -m pip install --user --upgrade --no-cache-dir -v \
+    ipywidgets \
     xformers
