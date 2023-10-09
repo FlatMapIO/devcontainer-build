6b6425e737568d0f5785d93b445e5aa93a8ea01f
dong.huo <huodon@gmail.com>
2023-10-09 12:48:37 +0800

    update 118

--- .devcontainer/features/pytorch/install.sh
+++ .devcontainer/features/pytorch/install.sh
@@ -5,10 +5,9 @@ user_exec() {
     su - vscode -c "$COMMAND"
 }
 
-
 user_exec ${PYTHON_PATH}/bin/python3 -m pip install --user --upgrade --no-cache-dir -v \
-    torch torchvision torchaudio
+    torch torchvision torchaudio --extra-index-url "${INDEXURL}" \
 
 user_exec ${PYTHON_PATH}/bin/python3 -m pip install --user --upgrade --no-cache-dir -v \
     ipywidgets \
-    xformers
+    'xformers<0.0.22'
