e0166442d83ab1d6a4710851c5d8679ca293e282
huodon <huodon@gmail.com>
2023-10-29 20:44:49 +0800

    WIP

--- .devcontainer/features/homebrew/install.sh
+++ .devcontainer/features/homebrew/install.sh
@@ -1,23 +1,23 @@
 user_exec() {
-    COMMAND="$*"
-    su - vscode -c "$COMMAND"
+	COMMAND="$*"
+	su - vscode -c "$COMMAND"
 }
-
 
 user_exec /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 ln -s /home/linuxbrew/.linuxbrew /opt/homebrew
 
-user_exec /opt/homebrew/bin/brew install \
+user_exec /opt/homebrew/bin/brew install
 
 ulimit -n 80000
 
 user_exec /opt/homebrew/bin/brew install \
-    git git-lfs \
-    ripgrep difftastic just nvim \
-    fish lsd bat htop aria2 fd \
-    ffmpeg
+	git git-lfs \
+	ripgrep difftastic nvim \
+	fish lsd bat htop aria2 fd \
+	ffmpeg
 
-user_exec /opt/homebrew/bin/brew cleanup  --prune=all
+user_exec /opt/homebrew/bin/brew cleanup --prune=all
 
 ls -l /opt/homebrew/Cellar
+
